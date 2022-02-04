4186d254591057ba74d4897fc06e1d0d880f8f01
Tomer-Gazit <tomergazit3004@gmail.com>
2022-02-04 23:49:01 +0200

    Made some progress, still testing.

--- cli.py
+++ cli.py
@@ -1,16 +1,40 @@
 import requests
 import json
 from getpass import getpass
+import os
 
 #enter credentials
 username = input("Username: ")
 password = getpass()
-artifactory = "https://tgz.jfrog.io/artifactory/" #artifactory URL
-api = "api/security/users" #you can change this API URL to any API method you'd like to use
 
-url = artifactory + api
-r = requests.get(url, auth = (username, password)) #this script is only for API methods that use GET
-print(username,password)
-print(r.status_code)
+# url to jfrog instance
+instance = "https://tgz.jfrog.io/"
+# api url
+api = "artifactory/api/security/users" 
+url = instance + api
+r = requests.get(url, auth = (username, password))
+
 if r.status_code == 200:
-    print(r.text)
+    # Check for cached token. Create one if doesn't exist.
+    print('Enter the path to the directory in which your .token_<username> file is saved.\n\
+        If you don\'t have such file, it will be created at the specified path.\
+        Please make sure the path is secure and known only to you.')
+    tokenPath = input('path: ')
+    if not os.path.exists(tokenPath):
+        os.makedirs(tokenPath)
+    with open((tokenPath+"/.token_"+username),"a+") as f:
+        f.seek(0)
+        if len(f.read()) == 0:
+            f.write(input('insert token: '))
+        f.seek(0)
+        token = f.read()
+    # 
+    bearer ='Bearer ' + token
+    header = {
+    'Authorization': bearer
+    }
+
+    woof = requests.get('https://tgz.jfrog.io/access/api/v1/tokens', headers = header)
+    print(woof.status_code,"\n",woof.text)
+else:
+   print('***\n',r.status_code)
