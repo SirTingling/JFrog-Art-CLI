fa4207e58daea6e0d060ff0ddc67d9d776914c31
Tomer-Gazit <tomergazit3004@gmail.com>
2022-02-06 19:03:03 +0200

    Don't forget to create an 'edit token' command

--- shell.py
+++ shell.py
@@ -4,8 +4,24 @@ from getpass import getpass
 
 shell = "no"
 
+#jfrog instance
+with open("cache/.instance", "a+") as f:
+    f.seek(0)
+    if not len(f.read()) == 0:
+        f.seek(0)
+        validateInstance = input(("Is your JFrog instance \"" + f.read() + "\" ? [Y/n]: "))
+        if validateInstance == "Y".casefold:
+            print("good")
+        elif validateInstance == "n".casefold():
+            f.truncate(0)
+            f.write(input("Please insert your instance: "))
+    else:
+        f.write(input("Please insert your instance: "))
+    f.seek(0)
+    instance = f.read()
+
 # CLI Username
-with open(".username", 'a+') as f:
+with open("cache/.username", 'a+') as f:
     f.seek(0)
     if not len(f.read()) == 0:
         f.seek(0)
@@ -23,7 +39,7 @@ with open(".username", 'a+') as f:
 #CLI Password
 password = getpass()
 # Validate login
-r = requests.get("https://tgz.jfrog.io/artifactory/api/security/users/"+username, auth=(username, password))
+r = requests.get(instance+"artifactory/api/security/users/"+username, auth=(username, password))
 if r.status_code == 401:
     print("Authentication denied [401].\nYour username or password might be incorrect.")
     exit(1)
@@ -59,21 +75,39 @@ def clearConsole():
     os.system(command)
 
 def confUsername():
-    username = open(".username", 'r').read()
+    username = open("cache/.username", 'r').read()
     validateConfUser = input("Current username is \""+username+"\", would you like to overwrite it? [y/N]: ")
     if validateConfUser in ('','n'.casefold()) :
         print("Username unchanged")
     elif validateConfUser == 'y'.casefold():
-        with open(".username", "a+") as f:
+        with open("cache/.username", "a+") as f:
             f.truncate(0)
             f.write(input("Please insert your username: "))
             f.seek(0)
             username = f.read()
         print("Username successfully changed to \""+username+"\"")
 
+def confInstance():
+    instance = open("cache/.instance", 'r').read()
+    validateConfInstance = input("Current instance is \""+instance+"\", would you like to overwrite it? [y/N]: ")
+    if validateConfInstance in ('','n'.casefold()) :
+        print("Username unchanged")
+    elif validateConfInstance == 'y'.casefold():
+        with open("cache/.instance", "a+") as f:
+            f.truncate(0)
+            f.write(input("Please insert your instance: "))
+            f.seek(0)
+            instance = f.read()
+        print("Instance successfully changed to \""+instance+"\"")
 
 def printUsername():
     print(username)
+
+def printInstance():
+    print(instance)
+
+def getSystemPing():
+    r = requests.get()
 
 def getHelp():
     print('Console Usage:\n'+('∙'*14))
@@ -89,10 +123,12 @@ def getHelp():
 functionsHelp = {
     'edit': {
         'username': 'Changes the username this CLI uses',
-        'token': 'Changes the access token this CLI uses'
+        'token': 'Changes the access token this CLI uses',
+        'instance': 'Change the instance this CLI runs requests against'
     },
     'print': {
-        'username': 'Prints the username this CLI uses'
+        'username': 'Prints the username this CLI uses',
+        'instance': 'Prints the isntance this CLI runs requests against'
     },
     'misc': {
         "clear": '''Clears console.
@@ -100,7 +136,10 @@ functionsHelp = {
             ''',
         "exit": '''Exits console.
             \tAlternative commands: quit, ex
-        '''
+        ''',
+        "help": '''Show this help message.
+            \tAlternative commands: h, info, inf
+            '''
     }
 }
 
@@ -109,7 +148,9 @@ shellFunctions = {
     'cls': clearConsole,
     'help': getHelp,
     'edit username': confUsername,
-    'print username': printUsername
+    'edit instance': confInstance,
+    'print username': printUsername,
+    'print instance': printInstance
 }
 
 
@@ -118,6 +159,11 @@ while shell == "yes":
     x = input ("():")
     if x in ('exit','exi','ex','quit'):
         break
+
+    elif x in ('h','info','inf'):
+        x = 'help'
+        if x in shellFunctions:
+            shellFunctions.get(x)()
 
     elif x in shellFunctions:
         shellFunctions.get(x)()
