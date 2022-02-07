3374af0d1a0a3e8c853be2c4ca4487b24769cb80
Tomer-Gazit <tomergazit3004@gmail.com>
2022-02-07 19:28:37 +0200

    it\'s pretty much done

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # JFrog-Art-CLI
+
 Solution to JFrog's home assignment
+
+
+## Workflow
+
+I had to research pretty much EVERYTHING.
+
+It is hard for me to cite sources, as my browsing history during this assignment had gotten **over 400 new entries**.
+
+Most of these entries are about python syntax, and are not specificly relevant to this assignment.
+
+The most prominent sources I used:
+
+1. [JFrog's official Documentation](https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API)
+2. [How to Generate an access Token](https://www.youtube.com/watch?v=OQ4_ZGCnqIo)
+3. [python requests docs](https://docs.python-requests.org/en/master/user/quickstart/)
+4. [stackoverflow](https://stackoverflow.com/), and specificly these threads:
+4. * [1](https://stackoverflow.com/questions/15785719/how-to-print-a-dictionary-line-by-line-in-python), [2](https://stackoverflow.com/questions/13782198/how-to-do-a-put-request-with-curl), [3](https://stackoverflow.com/questions/2967194/open-in-python-does-not-create-a-file-if-it-doesnt-exist).
+5. [DelfStack](https://www.delftstack.com/howto/python/python-clear-console/)
+
+I also used [this tool](https://curlconverter.com/#) quite a lot.
+
+### About some descisions I made during this project:
+
+* **Writing dirty** - The time constraint was too tight for me to both learn how to do it all, *and* to do it pretty. while dirty is bloated, it often is the *fastest* way to a result (and I neede *fast*).
+* **Creating an entire Shell** - A simple CLI is good when you only use it a small amounts of times in succession, but if you want to manage something that requires various variables, you don'y want to type them *every single time you run a command*; From a tool that helps you, a CLI can easily become jarring, and even harder to use. An interactive shell on the other hand, is ment for repetetive use.
+* **Inserting path to access token** - It allowed me to create a bit of a safer 'cache', without the complications of encryption (which would be another thing to learn).
+* **Repetitive password prompts** - Again, a security meassure. The password prompts are only used for commands which require an admin user.
+* **Instance and Username caching** - Hard coding credentials isn't a good idea, but if only a single user works with the shell, it feels much smoother to only confirm your identity instead of re-supplying it every time, especially during testing.
+
+
+
+## Console Usage:
+
+Just type 'help' in console.
+
+All commands are typed as their entire tree, except for commands in the 'misc' branch, which are not prefixed and only use the final command.
+
+So for e.g, if you want to see your system version, the command you type is
+> get system version
+
+Or if you want to delete a user:
+> DELETE user
+
+But if you want to clear the console you'd only have to type:
+> cls
+
+Some commands ask you for further input, and commands that require an administrator access always ask for password confirmation.
+Note that all misc commands are unprefixed
--- shell.py
+++ shell.py
@@ -39,7 +39,7 @@ with open("cache/.username", 'a+') as f:
 #CLI Password
 password = getpass()
 # Validate login
-r = requests.get(instance+"artifactory/api/security/users/"+username, auth=(username, password))
+r = requests.head (instance+"artifactory/api/security/users/"+username, auth=(username, password))
 if r.status_code == 401:
     print("Authentication denied [401].\nYour username or password might be incorrect.")
     exit(1)
@@ -58,8 +58,8 @@ with open((tokenPath+"/.token_"+username),"a+") as f:
         f.write(input('insert token: '))
     f.seek(0)
     token = f.read()
-header = {
-    'Authorization': 'Bearer' + token
+headers = {
+    'Authorization': 'Bearer ' + token
     }
 
 
@@ -74,19 +74,6 @@ def clearConsole():
         command = 'cls'
     os.system(command)
 
-def confUsername():
-    username = open("cache/.username", 'r').read()
-    validateConfUser = input("Current username is \""+username+"\", would you like to overwrite it? [y/N]: ")
-    if validateConfUser in ('','n'.casefold()) :
-        print("Username unchanged")
-    elif validateConfUser == 'y'.casefold():
-        with open("cache/.username", "a+") as f:
-            f.truncate(0)
-            f.write(input("Please insert your username: "))
-            f.seek(0)
-            username = f.read()
-        print("Username successfully changed to \""+username+"\"")
-
 def confInstance():
     instance = open("cache/.instance", 'r').read()
     validateConfInstance = input("Current instance is \""+instance+"\", would you like to overwrite it? [y/N]: ")
@@ -100,16 +87,121 @@ def confInstance():
             instance = f.read()
         print("Instance successfully changed to \""+instance+"\"")
 
+
 def printUsername():
     print(username)
 
 def printInstance():
     print(instance)
 
-def getSystemPing():
-    r = requests.get()
+def postCreateUser():
+    r
 
-def getHelp():
+def headSystemPing():
+    r = requests.head(instance+"access/api/v1/system/ping", headers=headers)
+    print(r)
+    if r.status_code == 200:
+        print("Ping Successfull")
+
+def getStorageInfo():
+    failcount = 0
+    passwordAuth = getpass()
+    while not passwordAuth == password:
+        print("Wrong password, try again")
+        passwordAuth = getpass()
+        failcount = failcount + 1
+        if failcount == 2:
+            failcount = 0
+            break
+    if passwordAuth == password:
+        r = requests.get(instance+"artifactory/api/storageinfo", headers=headers)
+        print(r,'\n',r.json())
+
+def getSystemVersion():
+    r = requests.get(instance+"artifactory/api/system/version", headers=headers)
+    print(r,'\n',r.text)
+
+
+def putCreateRepository(): ### Can't test this one
+    failcount = 0
+    passwordAuth = getpass()
+    while not passwordAuth == password:
+        print("Wrong password, try again")
+        passwordAuth = getpass()
+        failcount = failcount + 1
+        if failcount == 2:
+            failcount = 0
+            break
+    if passwordAuth == password:
+        repoKey = input('enter repository key: ')
+        jsonRepoConfPath = 'enter path to your repository-config.json file: '
+        jsonRepoConf = open(jsonRepoConfPath, 'r').read
+        data = "{"+jsonRepoConf+"}"
+        r = requests.put(instance+"artifactory/api/repositories/"+repoKey, headers = headers, data = data)
+        print(r,'\n',r.json())
+
+def deleteUser():
+    failcount = 0
+    passwordAuth = getpass()
+    while not passwordAuth == password:
+        print("Wrong password, try again")
+        passwordAuth = getpass()
+        failcount = failcount + 1
+        if failcount == 2:
+            failcount = 0
+            break
+    if passwordAuth == password:
+        userToDelete = input("!!! enter the user you want to DELETE: ")
+        r = requests.delete(instance+"artifactory/api/security/users/"+userToDelete, headers = headers)
+        print(r)
+
+
+def postCreateUser(): ### Couldn't get it to work, kept getting response 415
+    failcount = 0
+    passwordAuth = getpass()
+    while not passwordAuth == password:
+        print("Wrong password, try again")
+        passwordAuth = getpass()
+        failcount = failcount + 1
+        if failcount == 2:
+            failcount = 0
+            break
+    if passwordAuth == password:
+        print('insert new user details:')
+        createUserUsername = input('username: ')
+        createUserEmail = input('email: ')
+        createUserData = {
+        "schemas": [
+            "urn:ietf:params:scim:schemas:core:2.0:User"
+        ],
+        "userName": createUserUsername,
+        "active": True,
+        "emails": [
+            {
+            "value": createUserEmail,
+            "primary": True
+            }
+        ]
+        }
+        r = requests.post(instance+"access/api/v1/scim/v2/Users", headers = headers, data = createUserData)
+        print(r,'\n',r.json())
+
+def postExpirePassword():
+    failcount = 0
+    passwordAuth = getpass()
+    while not passwordAuth == password:
+        print("Wrong password, try again")
+        passwordAuth = getpass()
+        failcount = failcount + 1
+        if failcount == 2:
+            failcount = 0
+            break
+    if passwordAuth == password:
+        userToExpire = input('Enter a username to expire it\'s password: ')
+        r = requests.post(instance+'/api/security/users/authorization/expirePassword/'+userToExpire, headers = headers)
+        print(r,'\n',r.content)
+
+def miscHelp():
     print('Console Usage:\n'+('∙'*14))
     
     for a in functionsHelp:
@@ -121,14 +213,18 @@ def getHelp():
     print('*Note that all misc commands are unprefixed.')
 
 functionsHelp = {
-    'edit': {
-        'username': 'Changes the username this CLI uses',
-        'token': 'Changes the access token this CLI uses',
-        'instance': 'Change the instance this CLI runs requests against'
+    'DELETE': {
+        'user': 'Removes an Artifactory user.\n\t\t\tDANGEROUS.'
     },
-    'print': {
-        'username': 'Prints the username this CLI uses',
-        'instance': 'Prints the isntance this CLI runs requests against'
+    'edit': {
+        'instance': 'Change the instance this CLI runs requests against\n'
+    },
+    'get': {
+        'storage info': 'Returns storage summary information regarding binaries, file store and repositories\n',
+        'system version': 'Retrieve information about the current Artifactory version, revision, and currently installed Add-ons\n'
+    },
+    'head': {
+        'system ping': 'Sends a ping request to instance\n'
     },
     'misc': {
         "clear": '''Clears console.
@@ -140,17 +236,38 @@ functionsHelp = {
         "help": '''Show this help message.
             \tAlternative commands: h, info, inf
             '''
+    },
+    'print': {
+        'instance': 'Prints the isntance this CLI runs requests against\n',
+        'username': 'Prints the username this CLI uses\n'
+    },
+    'post': {
+        'create user': '''This API will create a single user
+            \t\tTHIS OPTION IS NOT WORKING
+        ''',
+        'expire password': 'Expires a user\'s password\n\t\t\tThis option could not be tested because i have no PRO access, but i believe it should work if i were to have access'
+    },
+    'put': {
+        'create repository': '''Creates a new repository in Artifactory with the provided configuration. Supported by local, remote, virtual and federated repositories
+            \t\t\tThis option could not be tested
+            '''
     }
 }
 
 shellFunctions = {
+    'DELETE user': deleteUser,
     'clear': clearConsole,
     'cls': clearConsole,
-    'help': getHelp,
-    'edit username': confUsername,
     'edit instance': confInstance,
+    'get storage info': getStorageInfo,
+    'get system version': getSystemVersion,
+    'head system ping': headSystemPing,
+    'help': miscHelp,
+    'print instance': printInstance,
     'print username': printUsername,
-    'print instance': printInstance
+    'post create user': postCreateUser,
+    'post expire password': postExpirePassword,
+    'put create repository': putCreateRepository
 }
 
 
